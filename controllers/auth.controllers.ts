870390a36c0ff7f0895919701a571ebcbbb3e20b
Nath <nathan.girardi@proton.me>
2025-04-21 11:00:34 +0200

    initialisation de prisma

--- controllers/auth.controllers.ts
+++ controllers/auth.controllers.ts
@@ -1,8 +1,28 @@
 import { Request, Response } from 'express'
+import prisma from '../database/prisma'
 
 
-export const signup = (req: Request, res: Response) => {
+export const signup = async (req: Request, res: Response) => {
 	const { username, firstName, email, password } = req.body
+
+	try {
+		if (!username || !email || !password || !firstName) {
+			return res.status(400).json({ message: "All fields are required" })
+		}
+
+		const existingUser = await prisma.user.findFirst({
+			where: {
+				OR: [
+					{ email },
+					{ username }
+				]
+			}
+		})
+
+
+	} catch (error) {
+
+	}
 }
 
 export const login = (req: Request, res: Response) => {
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import dotenv from "dotenv"
 import authRoutes from '../routes/auth.routes'
-import { connectDB } from '../database/connection'
 dotenv.config()
 
 const app = express()
@@ -10,7 +9,6 @@ app.use(express.json())
 
 app.use('/api/auth', authRoutes)
 
-connectDB()
 
 app.listen(8080, () => {
 	console.log("Server is running on port 8080")
